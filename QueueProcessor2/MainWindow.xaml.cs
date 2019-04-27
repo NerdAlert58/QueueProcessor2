1e50473b703ff1030b3324bca480555d6e32b2b4
NerdAlert58 <Bushyhead@gmail.com>
2019-04-26 22:51:59 -0500

    Initial process.

--- QueueProcessor2/MainWindow.xaml.cs
+++ QueueProcessor2/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using QueueProcessor2.Objects;
+using QueueProcessor2.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +25,20 @@ namespace QueueProcessor2
         public MainWindow()
         {
             InitializeComponent();
+
+            var processes = new List<Proc>()
+            {
+                new Proc() { Name = "P1", Color = Objects.Color.white, Priority = 1, Burst = 12, Arrival = 0, TurnAroundTime = -1, WaitTime = -1},
+                new Proc() { Name = "P2", Color = Objects.Color.blue, Priority = 2, Burst = 8, Arrival = 4, TurnAroundTime = -1, WaitTime = -1},
+                new Proc() { Name = "P3", Color = Objects.Color.purple, Priority = 1, Burst = 6, Arrival = 5, TurnAroundTime = -1, WaitTime = -1},
+                new Proc() { Name = "P4", Color = Objects.Color.green, Priority = 2, Burst = 5, Arrival = 12, TurnAroundTime = -1, WaitTime = -1},
+                new Proc() { Name = "P5", Color = Objects.Color.red, Priority = 2, Burst = 10, Arrival = 18, TurnAroundTime = -1, WaitTime = -1}
+            };
+
+            var handler = new Handler(processes);
+
+            var (events, results) = handler.DoWork();
+            Console.WriteLine("Pause here to check results.");
         }
     }
 }
